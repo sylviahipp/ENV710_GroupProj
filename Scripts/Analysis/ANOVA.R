35d6534704bb8415c9af3d1aa27855200a3a5043
maeve.gualtieri-reed@duke.edu <you@example.com>
2026-04-06 17:53:20 -0400

    ANOVA

--- Scripts/Analysis/ANOVA.R
+++ Scripts/Analysis/ANOVA.R
@@ -10,5 +10,20 @@ colnames(cvi_egrid_join)
 #asthma
 asthma_ANOVA <- aov(current_adult_asthma ~ Category, data = cvi_egrid_join)
 summary(asthma_ANOVA)
-
 TukeyHSD(asthma_ANOVA)
+
+#population
+pop_ANOVA <- aov(population ~ Category, data = cvi_egrid_join)
+summary(pop_ANOVA)
+TukeyHSD(pop_ANOVA)
+
+#railroad
+rail_ANOVA <- aov(rail_crossings ~ Category, data = cvi_egrid_join)
+summary(rail_ANOVA)
+TukeyHSD(rail_ANOVA)
+
+
+
+
+
+

634deca91f7186b80fec14c3a2806cb8334f3d1a
maeve.gualtieri-reed@duke.edu <you@example.com>
2026-04-08 12:09:08 -0400

    remove DC

--- Scripts/Analysis/ANOVA.R
+++ Scripts/Analysis/ANOVA.R
@@ -22,8 +22,18 @@ rail_ANOVA <- aov(rail_crossings ~ Category, data = cvi_egrid_join)
 summary(rail_ANOVA)
 TukeyHSD(rail_ANOVA)
 
+#poverty
+poverty_ANOVA <- aov(below_poverty ~ Category, data = cvi_egrid_join)
+summary(poverty_ANOVA)
+TukeyHSD(poverty_ANOVA)
 
+#hs diploma
+hs_ANOVA <- aov(no_high_school_diploma ~ Category, data = cvi_egrid_join)
+summary(hs_ANOVA)
+TukeyHSD(hs_ANOVA)
 
-
-
+#riverine_flooding_annualized_frequency
+riverine_ANOVA <- aov(riverine_flooding_annualized_frequency ~ Category, data = cvi_egrid_join)
+summary(riverine_ANOVA)
+TukeyHSD(riverine_ANOVA)
 
